ed0c9c4f2a91ab5e4a9f89340ae38939c557e9cc
William ji <williamji12345@gmail.com>
2020-09-20 01:30:47 -0400

    Updates

---
+++ precache-manifest.9d720497b6b2f0fbdccb28c858d4df8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24cde531cf932882d76bbee0809982e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a29468f5a2b68516007c",
+    "url": "/static/css/main.ad587ebb.chunk.css"
+  },
+  {
+    "revision": "a867f395c18a48e931c6",
+    "url": "/static/js/2.95ec1bc1.chunk.js"
+  },
+  {
+    "revision": "314bef60d3283ae1f9042152f2a97a1a",
+    "url": "/static/js/2.95ec1bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29468f5a2b68516007c",
+    "url": "/static/js/main.ad779631.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "21f18d717b3b20ab3f6cc7a0b523d2e4",
+    "url": "/static/media/backgroundVideo.21f18d71.mp4"
+  },
+  {
+    "revision": "74689cf4f91e036060d8299f272c3de3",
+    "url": "/static/media/login_back.74689cf4.jpg"
+  }
+]);
